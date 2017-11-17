1e671105d30e7006e3afce45f7f321783f613412
359211110116 <saowalak406beem@gmail.com>
2017-11-17 11:14:07 +0700

    successfuly information data

--- src/oop_lab/S359211110116/rmutsv/com/myFirstJAVA.java
+++ src/oop_lab/S359211110116/rmutsv/com/myFirstJAVA.java
@@ -4,6 +4,9 @@ public class myFirstJAVA {
 
     public static void main(String[] args) {
       System.out.println("Hello Wordl, I'm BEEM");
+      System.out.println("Infromation System");
+      System.out.println("Faculty of Management Technology");
+      System.out.println("RMUTSV");
     }// main
 
 
