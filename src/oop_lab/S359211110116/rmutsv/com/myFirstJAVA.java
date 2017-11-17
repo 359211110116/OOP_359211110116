a8034d30a3eae1e04037641708f4a68c3d4fa945
359211110116 <saowalak406beem@gmail.com>
2017-11-17 10:31:50 +0700

    Testing upload to Github

---
+++ src/oop_lab/S359211110116/rmutsv/com/myFirstJAVA.java
@@ -0,0 +1,13 @@
+package oop_lab.S359211110116.rmutsv.com;
+
+public class myFirstJAVA {
+
+    public static void main(String[] args) {
+      System.out.println("Hello Wordl, I'm BEEM");
+    }// main
+
+
+
+} // class
+
+
