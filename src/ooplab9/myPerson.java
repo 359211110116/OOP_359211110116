68fa08f6e400bbf26834309716152d5d7e7b8b63
359211110116 <Saowalak406beem@gmail.com>
2018-02-16 10:50:58 +0700

    myPerson

--- src/ooplab9/myPerson.java
+++ src/ooplab9/myPerson.java
@@ -1,4 +1,5 @@
 package ooplab9;
+//ออกสอบไฟนอล
 
 public class myPerson {
     public static void main(String[] args) {
@@ -6,6 +7,11 @@ public class myPerson {
                 new Address("104", "nakhonsrithammarat", "80180"),
                 new Job("receptionist", 10000));
         System.out.println(person.toString());
+
+        //edit data of objet
+        person.getJob().setSalary(10000);
+        System.out.println(person.getJob().getPosition());
+        System.out.println(person.getJob().getSalary());
 
 
     }//main
